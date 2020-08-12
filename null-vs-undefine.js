f6ec0ed49c1aebb83c35d093f3aa596fed535372
dewanNz <nuruzzaman.dn@gmail.com>
2020-08-12 16:12:38 +0600

    nul vs undefined

---
+++ null-vs-undefine.js
@@ -0,0 +1,21 @@
+// let myName;
+// console.log(myName);
+
+// function  add(num1, num2){
+//     console.log(num1 + num2);
+//     return 
+// }
+// const result = add(10, 20);
+// console.log(result);
+
+// const premik = { name: 'salman shah', phone:'0145455' }
+// console.log(premik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+//  null = not existant
+
+
+let ages = [12, 14, 15];
+console.log(ages[11]);
