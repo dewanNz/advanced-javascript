a279440aa5513155923e09b35c069a0aa30f0e72
dewanNz <nuruzzaman.dn@gmail.com>
2020-08-12 15:50:21 +0600

    learned ture-false

---
+++ ture-false.js
@@ -0,0 +1,35 @@
+// const age = 4;
+
+// if(age > 0){
+//     console.log('condition is ture');
+// } else {
+//     console.log('condition is false ');
+// }
+
+// const age = 4;
+
+// if(age){
+//     console.log('condition is ture');
+// } else {
+//     console.log('condition is false ');
+// }
+
+
+// 0
+// ""
+// undefined
+// null 
+// NaN
+// false
+// above all falsy
+
+
+// turthy
+// '0' , " ", emty array [], {}- object without property
+const name = "atik";
+
+if(name.length >0){
+    console.log('condition is ture');
+} else {
+    console.log('condition is false ');
+}
